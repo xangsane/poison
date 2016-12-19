541859a4176168ac35e0f01eb33bb46b1878c3f5
xangsane <gilbert.buslig@gmail.com>
2016-12-19 18:22:09 +0800

    sql dumps

--- create_dump.sql
+++ create_dump.sql
@@ -40,17 +40,3 @@ CREATE TABLE users (
   password chkpass NOT NULL,
 	PRIMARY KEY (username)
 );
-
-INSERT INTO bread values (1,'Japan No. 3',65.00,2);
-INSERT INTO bread values (2,'Japan No. 16',50.00,6);
-
-INSERT INTO history values ('joe',1,4,2,130.00,'2016-12-18 07:57:58');
-INSERT INTO history values ('joe',1,5,2,130.00,'2016-12-18 15:49:16');
-INSERT INTO history values ('joe',1,6,1,65.00,'2016-12-19 10:46:01');
-INSERT INTO history values ('joe',2,7,1,50.00,'2016-12-19 10:58:12');
-
-INSERT INTO item_cart values ('joe',1,1);
-INSERT INTO item_cart values ('joe',2,3);
-
-INSERT INTO users values ('admin','admin','Tokyo','admin@pantasia.jp',1,'poison');
-INSERT INTO users values ('joe','johnny','Atlanta','joe@mlb.com',0,'password');
